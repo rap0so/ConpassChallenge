af79f3fba86764f5d2364e3bca7b6c45acf22858
William Godoy <william.s.godoy53@gmail.com>
2019-09-15 21:06:27 -0300

    Linting

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import React, { Component } from 'react';
-import Home from './Pages/Home'
-import Navbar from './Components/Navbar'
-import { SET_NEW_MARKER, GET_ALL_MARKERS, UPDATE_MARKER } from './Actions/actionTypes'
+import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import {
+  SET_NEW_MARKER,
+  GET_ALL_MARKERS,
+  UPDATE_MARKER
+} from './Actions/actionTypes';
 
-import inspector from './Inspector'
-import setDots from './Dots'
-import './App.scss'
-import { connect } from 'react-redux'
+import inspector from './Inspector';
+import setDots from './Dots';
+import './App.scss';
+import { connect } from 'react-redux';
 
-const mapDispatchToProps = dispatch => ({
-  setNewMark: marker => dispatch({
-    type: SET_NEW_MARKER,
-    marker
-  }),
-  updateMarker: data => dispatch({
-    type: UPDATE_MARKER,
-    data
-  }),
-  getSavedMarkers: () => dispatch({
-    type: GET_ALL_MARKERS
-  })
-})
-const mapStateToProps = state => {
-  return {
-    markers: state.app.markers
-  }
-}
 export class App extends Component {
   constructor() {
-    super()
-
-    this.handleCreateHotspot = this.handleCreateHotspot.bind(this)
+    super();
+    this.handleCreateHotspot = this.handleCreateHotspot.bind(this);
   }
 
   /**
    * Handle all subfunctions relatives to first level hotspot creation
    */
   handleCreateHotspot() {
-    inspector.turnInspectorOn(this.props.setNewMark)
+    inspector.turnInspectorOn(this.props.setNewMark);
   }
   componentDidMount() {
-    this.props.getSavedMarkers()
-    setDots(this.props.markers, this.props.updateMarker)
+    this.props.getSavedMarkers();
+    setDots(this.props.markers, this.props.updateMarker);
   }
   componentDidUpdate() {
-    setDots(this.props.markers, this.props.updateMarker)
+    setDots(this.props.markers, this.props.updateMarker);
   }
   render() {
     return (
@@ -56,4 +41,28 @@ export class App extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+const mapDispatchToProps = dispatch => ({
+  setNewMark: marker =>
+    dispatch({
+      type: SET_NEW_MARKER,
+      marker
+    }),
+  updateMarker: data =>
+    dispatch({
+      type: UPDATE_MARKER,
+      data
+    }),
+  getSavedMarkers: () =>
+    dispatch({
+      type: GET_ALL_MARKERS
+    })
+});
+
+const mapStateToProps = state => ({
+  markers: state.app.markers
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/Reducers/markers.js
+++ src/Reducers/markers.js
@@ -1,66 +1,68 @@
 import {
-    SET_NEW_MARKER,
-    REMOVE_MARKER,
-    GET_ALL_MARKERS,
-    UPDATE_MARKER
+  SET_NEW_MARKER,
+  REMOVE_MARKER,
+  GET_ALL_MARKERS,
+  UPDATE_MARKER
 } from '../Actions/actionTypes.js';
 
-import localManager from '../LocalStorage'
+import localManager from '../LocalStorage';
 
 const defaultState = {
-    markers: []
-}
+  markers: []
+};
 const defaultAction = {
-    type: undefined
-}
+  type: undefined
+};
+
 export default (state = defaultState, action = defaultAction) => {
-    switch (action.type) {
-        case GET_ALL_MARKERS:
-            const savedHotspots = localManager.getAllHotspots()
-            return {
-                ...state, markers: [...state.markers, ...savedHotspots]
-            }
-        case SET_NEW_MARKER:
-            let newId = 0
-            if (state.markers.length) {
-                newId = Math.max(...state.markers.map(marker => marker.id))
-            }
-            const marker = {
-                id: newId + 1,
-                target: action.marker
-            }
-            let markers = [
-                ...state.markers, marker
-            ]
-            localManager.saveHotspot(markers)
+  switch (action.type) {
+    case GET_ALL_MARKERS:
+      const savedHotspots = localManager.getAllHotspots();
+      return {
+        ...state,
+        markers: [...state.markers, ...savedHotspots]
+      };
+    case SET_NEW_MARKER:
+      let newId = 0;
+      if (state.markers.length) {
+        newId = Math.max(...state.markers.map(marker => marker.id));
+      }
+      const marker = {
+        id: newId + 1,
+        target: action.marker
+      };
+      let markers = [...state.markers, marker];
+      localManager.saveHotspot(markers);
 
-            return {
-                ...state, markers
-            };
-        case UPDATE_MARKER:
-            const data = action.data
-            const sanitizedId = Number(data.id.replace(/\D+/g, ''))
-            let totalMarkers
-            
-            if (data) {
-                totalMarkers = state.markers.map(marker => {
-                    if (marker.id === sanitizedId) {
-                        marker.title = data.title || marker.title || undefined
-                        marker.description = data.description || marker.description || undefined
-                    }
-                    return marker;
-                });
-                localManager.saveHotspot(totalMarkers)
-            }
-            return { ...state, totalMarkers }
-        case REMOVE_MARKER:
-            const totalMarker = state.markers.filter(marker => {
-                return marker.id !== action.id;
-            });
-            localManager.saveHotspot(totalMarker)
+      return {
+        ...state,
+        markers
+      };
+    case UPDATE_MARKER:
+      const data = action.data;
+      const sanitizedId = Number(data.id.replace(/\D+/g, ''));
+      let totalMarkers;
 
-            return { ...state, markers: totalMarker }
-        default:
-            return state;
-    }
+      if (data) {
+        totalMarkers = state.markers.map(marker => {
+          if (marker.id === sanitizedId) {
+            marker.title = data.title || marker.title || undefined;
+            marker.description =
+              data.description || marker.description || undefined;
+          }
+          return marker;
+        });
+        localManager.saveHotspot(totalMarkers);
+      }
+      return { ...state, totalMarkers };
+    case REMOVE_MARKER:
+      const totalMarker = state.markers.filter(marker => {
+        return marker.id !== action.id;
+      });
+      localManager.saveHotspot(totalMarker);
+
+      return { ...state, markers: totalMarker };
+    default:
+      return state;
+  }
 };
